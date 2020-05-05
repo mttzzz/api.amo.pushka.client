d8000a9567406f99d8c020fe7802f543d52dbb55
di <dmitry.vasilevskiy@gmail.com>
2020-05-05 13:32:37 +0300

    1.8

---
+++ AmoPushkaClient.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace mttzzz\ApiAmocrmPushkaClient;
+
+
+use GuzzleHttp\Client;
+
+class AmoPushkaClient
+{
+    const BASE_URL = 'https://api.amocrm.pushka.biz/api/';
+    protected $guzzle;
+
+    public function __construct($key, $v = 'v2')
+    {
+        $this->guzzle = new Client(['base_uri' => self::BASE_URL . $v . '/','query' => ['key' => $key]]);
+    }
+
+    function __get($property)
+    {
+        $class = __NAMESPACE__ . '\Model';
+        return new $class($this->guzzle, $property);
+    }
+}
